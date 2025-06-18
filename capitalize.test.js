edd7c0c4f52a95f97fd3b9c217e4982cb63e70b9
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-18 15:04:54 +0530

    Add empty string test.

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,9 +1,13 @@
 import capitalize from "./capitalize.js";
 
-it("first letter capital", () => {
+it("first letter capital (1)", () => {
   expect(capitalize("kratos")).toBe("Kratos");
 });
 
-it("first letter capital", () => {
+it("first letter capital (2)", () => {
   expect(capitalize("sumo")).toBe("Sumo");
+});
+
+it("empty string", () => {
+  expect(capitalize("")).toBe("");
 });
