766111263d7d4eb40e5125c7908e78051671d23e
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-18 14:52:34 +0530

    Configure to use ES6 import statements and also use 'it' instead of 'test'.

--- sum.test.js
+++ sum.test.js
@@ -1,10 +1,10 @@
-const sum = require("./sum");
+import sum from "./sum.js";
 
-test("adds 1 + 2 to equal 3", () => {
+it("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
   expect(sum(0, 0)).toBe(0);
 });
 
-test("zero addition", () => {
+it("adds zeroes", () => {
   expect(sum(0, 0)).toBe(0);
 });
